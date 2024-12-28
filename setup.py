28c6ede34d1ccfcec06b83f447454792ce7726a5
Artemio A. Day Solvers <78178128+ArtemioA@users.noreply.github.com>
2024-12-28 19:10:26 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,62 +1,86 @@
-import io
+
+from setuptools import setup, find_packages
+
 import os
-from setuptools import setup
-from setuptools import find_packages
+from runpy import run_path
+import sys
 
-#https://widdowquinn.github.io/coding/update-pypi-package/
+here = os.path.abspath(os.path.dirname(__file__))
 
-def read(rel_path: str) -> str:
-    here = os.path.abspath(os.path.dirname(__file__))
-    # intentionally *not* adding an encoding option to open, See:
-    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
-    with open(os.path.join(here, rel_path)) as fp:
-        return fp.read()
+if sys.version_info[:2] < (2, 7) or (3, 0) <= sys.version_info[0:2] < (3, 3):
+    raise RuntimeError('Python version 2.7 or >= 3.3 required.')
 
-
-def get_version(rel_path: str) -> str:
-    for line in read(rel_path).splitlines():
-        if line.startswith("__version__"):
-            # __version__ = "0.11.2"
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    raise RuntimeError("Unable to find version string.")
-
-version = "1.3.0"#get_version("Artemix/__about__.py")
-
-with io.open("README.md", "r", encoding="utf-8") as f:
+# Get the long description from the relevant file
+with open(os.path.join(here, 'README.rst')) as f:
     long_description = f.read()
+lines = long_description.splitlines(True)
+long_description = ''.join(lines[26:])
 
-with open("requirements.txt", "r") as f:
-    install_requires = [x.strip() for x in f.readlines()]
+# Get the version from the relevant file
+d = run_path('fluiddyn/_version.py')
+__version__ = d['__version__']
 
-#
-setup(
-    name="Breadcrumbsfluidsim",
-    version=version,
-    description="A library for Fluid Mechanics Edited",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Artemio Araya",
-    author_email="artemioaday@gmail.com",
-    url="",
-    download_url="",
-    license="MIT",
-    install_requires=install_requires,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Topic :: Scientific/Engineering :: Mathematics",
-    ],
-    keywords=[
-        "Fluid Mechanics in Google Colab",
-    ],
-    packages=find_packages(),
-    include_package_data=True,
-)
+# Get the development status from the version string
+if 'a' in __version__:
+    devstatus = 'Development Status :: 3 - Alpha'
+elif 'b' in __version__:
+    devstatus = 'Development Status :: 4 - Beta'
+else:
+    devstatus = 'Development Status :: 5 - Production/Stable'
+
+
+install_requires = [
+    'numpy', 'matplotlib', 'h5py', 'h5netcdf', 'psutil >= 5.2.1', 'future',
+    'pathlib2; python_version<"3.4"', 'distro; python_version>="3.8"',
+]
+# Even though we also use scipy, we don't require its installation
+# because it can be heavy to install.
+
+# subprocess32 should not be used on Windows and should not be
+# a required dependency
+if not sys.platform.startswith('win') and sys.version_info[0] < 3:
+    install_requires.append('subprocess32')
+
+
+setup(name='fluiddyn',
+      version=__version__,
+      description=('framework for studying fluid dynamics.'),
+      long_description=long_description,
+      keywords='Fluid dynamics, research',
+      author='Pierre Augier',
+      author_email='pierre.augier@legi.cnrs.fr',
+      url='https://bitbucket.org/fluiddyn/fluiddyn',
+      license='CeCILL-B',
+      classifiers=[
+          # How mature is this project? Common values are
+          # 3 - Alpha
+          # 4 - Beta
+          # 5 - Production/Stable
+          devstatus,
+          'Intended Audience :: Science/Research',
+          'Intended Audience :: Education',
+          'Topic :: Scientific/Engineering',
+          'License :: OSI Approved :: BSD License',
+          # actually CeCILL-B License (BSD compatible license for French laws)
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Cython',
+          'Programming Language :: C'],
+      packages=find_packages(exclude=['doc']),
+      install_requires=install_requires,
+      scripts=['bin/fluidconvertim7'],
+      entry_points={
+          'console_scripts':
+          ['fluidinfo = fluiddyn.util.info:main',
+           'fluidnbstripout = fluiddoc.fluidnbstripout:main',
+           'fluiddocset = fluiddoc.fluiddocset:main',
+           'fluiddump = fluiddyn.io.dump:main',
+           'fluidmat2py = fluiddyn.util.matlab2py:main',
+           'fluidcluster-help = fluiddyn.clusters:print_help_scheduler']},
+      extras_require=dict(
+          doc=['Sphinx>=1.1', 'numpydoc']))
